1ab7293456abc3edb86f734423467c2088260f4a
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2020-08-17 22:57:14 +0200

    Implement a cursor-like Parser struct

--- src/libvamc_parser/src/definitions/mod.rs
+++ src/libvamc_parser/src/definitions/mod.rs
@@ -1,1 +1,13 @@
-mod ast;
+pub mod ast;
+
+use vamc_errors::Diagnostic;
+use vamc_lexer::definitions::*;
+
+pub type ParserResult<T> = Result<T, Diagnostic>;
+
+pub struct Parser {
+    /// The tokens from the lexer.
+    pub tokens: Vec<Token>,
+    /// The current token's index in tokens.
+    pub index: usize,
+}
--- src/libvamc_parser/src/lib.rs
+++ src/libvamc_parser/src/lib.rs
@@ -1,1 +1,18 @@
 mod definitions;
+use vamc_lexer::definitions::Token;
+
+use crate::definitions::*;
+
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Parser {
+        Parser { tokens, index: 0 }
+    }
+
+    pub fn bump(&mut self) {
+        self.index += 1;
+    }
+
+    pub fn token(&self) -> &Token {
+        &self.tokens[self.index]
+    }
+}
