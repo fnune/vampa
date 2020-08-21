88c5c2eadd1d826eeac214a4e9af95c97980ef49
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2020-08-21 12:47:46 +0200

    Terminate the basic block in with_compiler test util
    And then assert that the resulting function value is valid

--- src/libvamc_llvm/src/tests/util.rs
+++ src/libvamc_llvm/src/tests/util.rs
@@ -7,6 +7,9 @@ use crate::definitions::*;
 /// Builds a valid compiler with a block in which to write unit tests and
 /// verifies the resulting function value. A failure to verify the function
 /// value will result in a failing test. This is desired.
+///
+/// If the test function does not mutate the target block by adding a
+/// terminator, this test will add a dummy terminator for convenience.
 pub fn with_compiler<F: FnOnce(Compiler, BasicBlock, FunctionValue) -> ()>(test: F) {
     let context = Context::create();
     let module = context.create_module("test_module");
@@ -32,4 +35,13 @@ pub fn with_compiler<F: FnOnce(Compiler, BasicBlock, FunctionValue) -> ()>(test:
         .append_basic_block(function_value, "test_block");
 
     test(compiler, target_block, function_value);
+
+    // Append a terminator instruction for convenience.
+    // The user can overwrite it, and then this won't run.
+    if target_block.get_terminator().is_none() {
+        builder.position_at_end(target_block);
+        builder.build_return(Some(&context.i32_type().const_int(42, false)));
+    };
+
+    assert!(function_value.verify(true));
 }
