c04c75155a79afa226a122c32df33529ee3b774e
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2020-08-31 22:46:13 +0200

    Add calls to the list of exprs that can be compiled

--- src/libvamc_llvm/src/expression.rs
+++ src/libvamc_llvm/src/expression.rs
@@ -7,6 +7,9 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
     pub fn compile_expression(&self, expression: Expression) -> CompilerResult<BasicValueEnum> {
         match expression.kind {
             ExpressionKind::Literal(literal) => self.compile_literal(literal),
+            ExpressionKind::FunctionCall(function_name, function_parameters) => {
+                self.compile_function_call(function_name.as_str(), function_parameters)
+            },
             _ => unimplemented!(),
         }
     }
