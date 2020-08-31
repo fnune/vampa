c1a160c388d897474011b2861a5edd8afc8aefa6
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2020-09-01 00:18:28 +0200

    Implement a basic compile_source_file function

--- src/libvamc_llvm/src/source_file.rs
+++ src/libvamc_llvm/src/source_file.rs
@@ -1,7 +1,18 @@
-use crate::definitions::{Compiler, CompilerResult};
+use crate::definitions::Compiler;
+
+use vamc_parser::definitions::ast::SourceFile;
 
 impl<'a, 'ctx> Compiler<'a, 'ctx> {
-    pub fn compile_source_file(&mut self) -> CompilerResult<()> {
-        unimplemented!();
+    pub fn compile_source_file(&mut self, source_file: SourceFile) {
+        let target_block = self.context.append_basic_block(
+            *self
+                .function_value
+                .expect("Expected a function value in the compiler but found None."),
+            &source_file.file_name,
+        );
+
+        source_file.statements.into_iter().for_each(|statement| {
+            self.compile_statement(target_block, *statement);
+        });
     }
 }
