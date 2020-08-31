e06820106973acfaeea0b85e5b1618f17534e58c
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2020-08-31 22:57:49 +0200

    Implement matching against statement kinds and compiling

--- src/libvamc_llvm/src/statement.rs
+++ src/libvamc_llvm/src/statement.rs
@@ -1,7 +1,23 @@
-use crate::definitions::{Compiler, CompilerResult};
+use crate::definitions::Compiler;
+
+use inkwell::basic_block::BasicBlock;
+use vamc_parser::definitions::ast::{Statement, StatementKind};
 
 impl<'a, 'ctx> Compiler<'a, 'ctx> {
-    pub fn compile_statement(&mut self) -> CompilerResult<()> {
-        unimplemented!();
+    /// Does not return anything. It's here for the side effects.
+    pub fn compile_statement(&mut self, target_block: BasicBlock, statement: Statement) {
+        match statement.kind {
+            StatementKind::VariableDeclaration(variable_declaration) => {
+                self.compile_variable_declaration(target_block, variable_declaration)
+                    .unwrap();
+            },
+            StatementKind::FunctionDeclaration(function_declaration) => {
+                self.compile_function_declaration(function_declaration)
+                    .unwrap();
+            },
+            StatementKind::Return(statement_expression) => {
+                self.compile_expression(*statement_expression).unwrap();
+            },
+        }
     }
 }
