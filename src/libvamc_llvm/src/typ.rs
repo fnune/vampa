6b9e520176e7e7fc6fcde9877c8bbe6fed76f46a
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2020-09-15 18:04:45 +0200

    Implement basic compilation of types

--- src/libvamc_llvm/src/typ.rs
+++ src/libvamc_llvm/src/typ.rs
@@ -1,7 +1,16 @@
 use crate::definitions::{Compiler, CompilerResult};
 
+use inkwell::types::{BasicType, BasicTypeEnum};
+use vamc_errors::Diagnostic;
+use vamc_parser::definitions::ast::{IntType, Typ, TypKind};
+
 impl<'a, 'ctx> Compiler<'a, 'ctx> {
-    pub fn compile_typ(&mut self) -> CompilerResult<()> {
-        unimplemented!();
+    pub fn compile_typ(&self, typ: &Typ) -> CompilerResult<BasicTypeEnum<'ctx>> {
+        match &typ.kind {
+            TypKind::Int(int_type) => match int_type {
+                IntType::I32 => Ok(self.context.i32_type().as_basic_type_enum()),
+            },
+            _ => Err(Diagnostic::error("Only int types are supported.".into())),
+        }
     }
 }
