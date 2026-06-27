d547f0a6f276bbf60603b14bc660e0b51b8d2208
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2026-06-27 16:18:05 +0200

    Add nix fmt via treefmt and pin nixpkgs to 26.05

--- flake.nix
+++ flake.nix
@@ -2,17 +2,31 @@
   description = "Vampa: a toy programming language compiled with LLVM";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, rust-overlay, crane, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      crane,
+      flake-utils,
+      treefmt-nix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -27,20 +41,35 @@
         commonArgs = {
           src = pkgs.lib.cleanSourceWith {
             src = ./.;
-            filter = path: type:
-              (pkgs.lib.hasSuffix ".snap" path) || (craneLib.filterCargoSources path type);
+            filter = path: type: (pkgs.lib.hasSuffix ".snap" path) || (craneLib.filterCargoSources path type);
           };
           strictDeps = true;
 
           nativeBuildInputs = [ llvm.dev ];
-          buildInputs = with pkgs; [ libffi libxml2 ncurses zlib ];
+          buildInputs = with pkgs; [
+            libffi
+            libxml2
+            ncurses
+            zlib
+          ];
 
           "LLVM_SYS_221_PREFIX" = llvm.dev;
         };
 
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
         vamc = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
-      in {
+
+        treefmt = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.rustfmt.enable = true;
+          programs.nixfmt.enable = true;
+          programs.prettier.enable = true;
+        };
+      in
+      {
+        formatter = treefmt.config.build.wrapper;
+        checks.formatting = treefmt.config.build.check self;
+
         checks.vamc = vamc;
         packages.default = vamc;
         apps.default = flake-utils.lib.mkApp {
@@ -52,5 +81,6 @@
           packages = commonArgs.buildInputs ++ [ llvm.dev ];
           "LLVM_SYS_221_PREFIX" = llvm.dev;
         };
-      });
+      }
+    );
 }
